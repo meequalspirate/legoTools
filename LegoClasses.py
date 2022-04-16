75c9218b13d0bad849ed86545a654fddcecd73c6
meequalspirate <meequalspirate@gmail.com>
2022-04-15 21:52:52 -0400

    Added code and icon to account for parts with no image

--- LegoClasses.py
+++ LegoClasses.py
@@ -4,132 +4,8 @@ from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageTk
 import requests
-
-bricklinkColorDict = {'white' : 1, 
-             'very light gray' : 49,
-             'very light bluish gray' : 99,
-             'light bluish gray' : 86,
-             'light gray' : 9,
-             'dark gray' : 10,
-             'dark bluish gray' : 85,
-             'black' : 11,
-             'dark red' : 59,
-             'red' : 5,
-             'rust' : 27,
-             'coral' : 220,
-             'dark salmon' : 231,
-             'salmon' : 25,
-             'light salmon' : 26,
-             'sand red' : 58,
-             'reddish brown' : 88,
-             'brown' : 8,
-             'dark brown' : 120,
-             'dark tan' : 69,
-             'tan' : 2,
-             'light flesh' : 90,
-             'flesh' : 28,
-             'medium dark flesh' : 150,
-             'dark nougat' : 225,
-             'dark flesh' : 91,
-             'fabuland brown' : 106,
-             'fabuland orange' : 160,
-             'earth orange' : 29,
-             'dark orange' : 68,
-             'neon orange' : 165,
-             'orange' : 4,
-             'medium orange' : 31,
-             'bright light orange' : 110,
-             'light orange' : 32,
-             'very light orange' : 96,
-             'dark yellow' : 161,
-             'yellow' : 3,
-             'bright light yellow' : 103,
-             'light yellow' : 33,
-             'light lime' : 35,
-             'yellowish green' : 158,
-             'neon green' : 166,
-             'medium lime' : 76,
-             'lime' : 34,
-             'olive green' : 155,
-             'dark green' : 80,
-             'green' : 6,
-             'bright green' : 36,
-             'medium green' : 37,
-             'light green' : 38,
-             'sand green' : 48,
-             'dark turqoise' : 39,
-             'light turqoise' : 40,
-             'aqua' : 41,
-             'light aqua' : 152,
-             'dark blue' : 63,
-             'blue' : 7,
-             'dark azure' : 153,
-             'medium azure' : 156,
-             'medium blue' : 42,
-             'maersk blue' : 72,
-             'bright light blue' : 105,
-             'light blue' : 62,
-             'sky blue' : 87,
-             'sand blue' : 55,
-             'blue-violet' : 97,
-             'dark blue-violet' : 109,
-             'violet' : 43,
-             'medium violet' : 73,
-             'light violet' : 44,
-             'dark purple' : 89,
-             'purple' : 24,
-             'light purple' : 93,
-             'medium lavender' : 157,
-             'clikits lavender' : 227,
-             'lavender' : 154,
-             'sand purple' : 54,
-             'magenta' : 71,
-             'dark pink' : 47,
-             'medium dark pink' : 94,
-             'bright pink' : 104,
-             'pink' : 23,
-             'light pink' : 56,
-             'trans-clear' : 12,
-             'trans-black' : 13,
-             'trans-red' : 17,
-             'trans-neon orange' : 18,
-             'trans-orange' : 98,
-             'trans-light orange' : 164,
-             'trans-neon yellow' : 121,
-             'trans-yellow' : 19,
-             'trans-neon green' : 16,
-             'trans-bright green' : 108,
-             'trans-light green' : 221,
-             'trans-light bright green' : 226,
-             'trans-green' : 20,
-             'trans-dark blue' : 14,
-             'trans-medium blue' : 74,
-             'trans-light blue' : 15,
-             'trans-aqua' : 113,
-             'trans-light purple' : 114,
-             'trans-purple' : 51,
-             'trans-dark pink' : 50,
-             'trans-pink' : 107,
-             'chrome gold' : 21,
-             'chrome silver' : 22,
-             'chrome antique brass' : 57,
-             'chrome black' : 122,
-             'chrome blue' : 52,
-             'chrome green' : 64,
-             'chrome pink' : 82,
-             'pearl white' : 83,
-             'pearl very light gray' : 119,
-             'pearl light gray' : 66,
-             'flat silver' : 95,
-             'pearl dark gray' : 77,
-             'pearl light gold' : 61,
-             'pearl gold' : 115,
-             'flat dark gold' : 81,
-             'copper' : 84,
-             'metal blue' : 78,
-             'metallic silver' : 67,
-             'metallic green' : 70,
-             'metallic gold' : 65}
+import os
+import sys
 
 class LegoElement:
     name = ""
@@ -148,11 +24,21 @@ class LegoElement:
         self.color = color
         self.colorId = colorId
         self.bricklinkColorId = bricklinkColorId
-        self.imgPath = imgPath
-        self.image = Image.open(requests.get(self.imgPath, stream=True).raw)
+        if( imgPath != None and "http" in imgPath ) :
+            self.imgPath = imgPath
+            self.image = Image.open(requests.get(self.imgPath, stream=True).raw)
+        else:
+            self.imgPath = self.getPath("./x.png")
+            self.image = Image.open(self.imgPath)
         self.image = self.image.resize((85,85))
         self.photo = ImageTk.PhotoImage(self.image)
         self.currentAmount = currentAmount
+    
+    def getPath(self, filename):
+        if hasattr(sys, "_MEIPASS"):
+            return os.path.join(sys._MEIPASS, filename)
+        else:
+            return filename
 
 class RebrickableHtmlTableParser(HTMLParser):
     dataOrder = ['partNumber', 'requiredAmount', 'color', 'name']
